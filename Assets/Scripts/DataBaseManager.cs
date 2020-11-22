3d25d23030e869087d1aeee50b0cc6a427f51fbd
Hamze <hamzaalrashed1@hotmail.com>
2020-11-22 18:11:58 +0100

    version merged

--- Assets/Scripts/DataBaseManager.cs
+++ Assets/Scripts/DataBaseManager.cs
@@ -34,7 +34,9 @@ public static class DataBaseManager
         SkillGroupNameToEdit = null;
     }
 
-    
+    public static string ChosenUserGroup;
+
+    public static string ChosenSkillGroup;
     
     
     
